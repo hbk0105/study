efb41e0c552764f6df0a6e1e5d105dc61f4dbfb4
hbk0105 <adsfdas@naver.com>
2020-12-30 13:16:02 +0900

    수정

--- src/main/java/com/boot/study/StudyApplication.java
+++ src/main/java/com/boot/study/StudyApplication.java
@@ -34,8 +34,7 @@ public class StudyApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringApplicationBuilder.class, args);
 	}*/
-/*
-
+/* 
 	@Bean
 	DispatcherServlet dispatcherServlet () {
 		DispatcherServlet ds = new DispatcherServlet();
